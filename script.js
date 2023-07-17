e2870fafd16bd55445f76ec73949e28d7d3dc578
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-17 19:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function calculateMinCost() {
-  //your code here
   
-  
-  
-}  
+  const inputElement = document.getElementById('input');
+  const resultDiv = document.getElementById('result');
+
+  const input = inputElement.value;
+  const lengths = input.split(',').map(Number);
+
+  const priorityQueue = new MinPriorityQueue();
+
+  for (let length of lengths) {
+    priorityQueue.enqueue(length);
+  }
+
+  let totalCost = 0;
+
+  while (priorityQueue.size() > 1) {
+    
+    const smallest1 = priorityQueue.dequeue().element;
+    const smallest2 = priorityQueue.dequeue().element;
+
+    const cost = smallest1 + smallest2;
+
+    totalCost += cost;
+
+    priorityQueue.enqueue(cost);
+  }
+
+  resultDiv.textContent = totalCost;
+}
